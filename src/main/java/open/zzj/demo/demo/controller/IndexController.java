85ddff0bd405c6de67f5e36afed0d5c4534970ee
a1573798501 <1573798501@qq.com>
2019-11-08 15:38:08 +0800

    add index and bootstrap

--- src/main/java/open/zzj/demo/demo/controller/IndexController.java
+++ src/main/java/open/zzj/demo/demo/controller/IndexController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    //public String hello(@RequestParam(name = "name") String name, Model model){
+        //model.addAttribute("name",name);
+    public String index(){
+        return "index";
     }
 
 }
