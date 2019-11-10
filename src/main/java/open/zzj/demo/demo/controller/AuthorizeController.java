b444bfe231276427f66f7d4ad770d1eea6eda55b
a1573798501 <1573798501@qq.com>
2019-11-10 14:38:32 +0800

    add database function

--- src/main/java/open/zzj/demo/demo/controller/AuthorizeController.java
+++ src/main/java/open/zzj/demo/demo/controller/AuthorizeController.java
@@ -3,13 +3,20 @@ package open.zzj.demo.demo.controller;
 
 import open.zzj.demo.demo.dto.AccessTokenDTO;
 import open.zzj.demo.demo.dto.GithubUser;
+import open.zzj.demo.demo.mapper.UserMapper;
+import open.zzj.demo.demo.model.User;
 import open.zzj.demo.demo.provider.GithubProvider;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,9 +33,13 @@ public class AuthorizeController {
     @Value("${github.redirect.url}")
     private String redirectUrl;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
@@ -37,9 +48,23 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-//        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登陆成功，写cookie和session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登陆
+            return "redirect:/";
+        }
     }
 
 }
---
+++ src/main/java/open/zzj/demo/demo/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package open.zzj.demo.demo.mapper;
+
+
+import open.zzj.demo.demo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) values " +
+            "(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+
+}
